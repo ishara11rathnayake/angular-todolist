923c0576bc2510fbf24e02a6ab6233c33cfa2f33
ishara11rathnayake <ishara11rathnayake@gmail.com>
2019-06-11 20:57:43 +0530

    completed initial design

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Todo } from "../../models/Todo";
 
 @Component({
-  selector: 'app-todos',
-  templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  selector: "app-todos",
+  templateUrl: "./todos.component.html",
+  styleUrls: ["./todos.component.css"]
 })
 export class TodosComponent implements OnInit {
-
-  constructor() { }
+  todos: Todo[];
+  constructor() {}
 
   ngOnInit() {
+    this.todos = [
+      {
+        id: 1,
+        title: "Todo One",
+        completed: false
+      },
+      {
+        id: 1,
+        title: "Todo Two",
+        completed: true
+      },
+      {
+        id: 1,
+        title: "Todo Three",
+        completed: false
+      }
+    ];
   }
-
 }
